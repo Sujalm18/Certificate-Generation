499dc1290a9cefe4b7f9a6fed185bfbe78e74f8d
Sujal Mandape <115067933+Sujalm18@users.noreply.github.com>
2025-11-24 11:07:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ if logo_path.exists():
         img = Image.open(logo_path)
 
         # Create 3 equal columns and put the logo in the center one
-        col1, col2, col3 = st.columns([1, 0.5, 1])
+        col1, col2, col3 = st.columns([1, 0, 1])
         with col2:
-            st.image(img, width=100)   # adjust size here (40–70 recommended)
+            st.image(img, width=120)   # adjust size here (40–70 recommended)
 
     except Exception as e:
         st.warning(f"Logo found but could not be displayed: {e}")
